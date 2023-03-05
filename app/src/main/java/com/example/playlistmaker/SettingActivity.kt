60a34198eafe9712c4447422e9efc5540e460fb6
Rinat <eenot84@yanex.ru>
2023-03-05 16:53:11 +0400

    Added transition between screens.

---
+++ app/src/main/java/com/example/playlistmaker/SettingActivity.kt
@@ -0,0 +1,18 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+        val butBack = findViewById<Button>(R.id.button_setting_back)
+        butBack.setOnClickListener {
+            val intentSettingBack = Intent(this, MainActivity::class.java)
+            startActivity(intentSettingBack)
+        }
+    }
+}
